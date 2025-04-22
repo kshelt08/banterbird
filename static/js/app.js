dffead7e7b05c17ff953bc8181ef778c901a81a5
kshelt08 <kayleefs08@gmail.com>
2025-04-22 11:32:25 -0500

    Tweeting
    tweeting to the page

--- static/js/app.js
+++ static/js/app.js
@@ -7,10 +7,23 @@ function renderPost(post) {
     document.getElementById("feed").appendChild(template);
 }
 
-function submitPost() {
+async function submitPost() {
     const message = document.getElementById("postInput").value;
-    console.log("Would post:", message);
-    alert("Tweet submitted (not really yet)");
+    console.log(message, username);
+    try{
+        const response = await fetch("/api/posts", {
+            method: "POST", 
+            headers: {
+                "Content-Type": "application/json",
+            },
+            body: JSON.stringify({
+                username: username, 
+                message: message,
+            }),
+        });
+    } catch(error) {
+        console.error("Post failed😭", error)
+    }
 }
 
 window.onload = async () => {
