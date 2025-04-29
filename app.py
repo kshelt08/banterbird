dd386b74e5c6d204cd627e7322fa39e726a7aadc
kshelt08 <kayleefs08@gmail.com>
2025-04-29 10:08:42 -0500

    Login

--- app.py
+++ app.py
@@ -7,6 +7,11 @@ app = Flask(__name__)
 def index():
     return render_template("index.html")
 
+@app.route('/login')
+def login():
+    return render_template("login.html")
+
+
 @app.route('/api/posts')
 def get_posts():
     with open ('post.json', 'r') as file:
